db4403f5321c78ae95ae7a2573044180c725e401
Papah-Chan <92633211+FahriAdison@users.noreply.github.com>
2024-05-09 05:59:06 +0700

    NEW: Pengingat Sholat

---
+++ plugins/_auto-sholat.js
@@ -0,0 +1,43 @@
+export async function before(m) {
+    this.autosholat = this.autosholat ? this.autosholat : {}
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
+    let id = m.chat
+    if (id in this.autosholat) {
+        return false
+    }
+      let jadwalSholat = {
+      Fajr: "04:30",
+      Sunrise: "05:57",
+      Dhuhr: "11:49",
+      Asr: "15:03",
+      Sunset: "17:54",
+      Maghrib: "17:50",
+      Isha: "19:00",
+      Imsak: "04:30",
+      Midnight: "23:55",
+      Firstthird: "21:54",
+      Lastthird: "01:55"
+    }
+    const date = new Date((new Date).toLocaleString("en-US", {
+        timeZone: "Asia/Jakarta"
+    }));
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const timeNow = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+    for (const [sholat, waktu] of Object.entries(jadwalSholat)) {
+        if (timeNow === waktu) {
+        let caption = `Hai kak @${who.split`@`[0]},\nWaktu *${sholat}* telah tiba, ambilah air wudhu dan segeralah shalat🙂.\n\n*${waktu}*\n_untuk wilayah Jakarta dan sekitarnya._`
+            this.autosholat[id] = [
+                this.reply(m.chat, caption, null, {
+    contextInfo: {
+      mentionedJid: [who]
+    }
+  }),
+                setTimeout(() => {
+                    delete this.autosholat[id]
+                }, 57000)
+            ]
+        }
+    }
+}
+export const disabled = false
