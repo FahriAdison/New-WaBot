c36a2a55b0c8d0e883da946fba28a9a64297e400
Papah-Chan <92633211+FahriAdison@users.noreply.github.com>
2024-05-10 07:54:50 +0700

    CHANGE: Check SaveFile Jika Ada Syntax Yang Error'

--- plugins/owner-sv.js
+++ plugins/owner-sv.js
@@ -1,14 +1,68 @@
 import fs from 'fs'
-let handler = async (m, { text, usedPrefix, command }) => {
-    if (!text) throw `uhm.. teksnya mana?\n\npenggunaan:\n${usedPrefix + command} <teks>\n\ncontoh:\n${usedPrefix + command} plugins/melcanz.js`
-    if (!m.quoted.text) throw `balas pesan nya!`
-    let path = `${text}`
-    await fs.writeFileSync(path, m.quoted.text)
-    m.reply(`tersimpan di ${path}`)
+import syntaxError from 'syntax-error'
+import path from 'path'
+import util from 'util'
+
+const _fs = fs.promises
+
+let handler = async (m, { text, usedPrefix, command, __dirname }) => {
+    if (!text) throw `
+Penggunaan: ${usedPrefix}${command} <name file>
+Contoh: ${usedPrefix}savefile main.js
+        ${usedPrefix}saveplugin owner
+`.trim()
+    if (!m.quoted) throw `Reply Kodenya`
+    if (/p(lugin)?/i.test(command)) {
+        let filename = text.replace(/plugin(s)\//i, '') + (/\.js$/i.test(text) ? '' : '.js')
+        const error = syntaxError(m.quoted.text, filename, {
+            sourceType: 'module',
+            allowReturnOutsideFunction: true,
+            allowAwaitOutsideFunction: true
+        })
+        if (error) throw error
+        const pathFile = path.join(__dirname, filename)
+        // TODO: make confirmation to save if file already exists
+        // if (fs.existSync(pathFile, fs.constants.R_OK)) return m.reply(`File ${filename} sudah ada`)
+        await _fs.writeFile(pathFile, m.quoted.text)
+        m.reply(`
+Sukses Menyimpan Di *${filename}*
+
+\`\`\`
+${util.format(m.quoted.text)}
+\`\`\`
+`.trim())
+    } else {
+        const isJavascript = m.quoted.text && !m.quoted.mediaMessage && /\.js/.test(text)
+        if (isJavascript) {
+            const error = syntaxError(m.quoted.text, text, {
+                sourceType: 'module',
+                allowReturnOutsideFunction: true,
+                allowAwaitOutsideFunction: true
+            })
+            if (error) throw error
+            await _fs.writeFile(text, m.quoted.text)
+            m.reply(`
+Sukses Menyimpan Di *${text}*
+
+\`\`\`
+${util.format(m.quoted.text)}
+\`\`\`
+`.trim())
+        } else if (m.quoted.mediaMessage) {
+            const media = await m.quoted.download()
+            await _fs.writeFile(text, media)
+            m.reply(`
+Sukses Menyimpan Di *${text}*
+`.trim())
+        } else {
+            throw 'Tidak Support!!'
+        }
+    }
 }
-handler.help = ['sf'].map(v => v + ' <teks>')
+handler.help = ['saveplugin']
 handler.tags = ['owner']
-handler.command = /^sf$/i
+handler.command = /^(sf|saveplugin)$/i
 
 handler.rowner = true
+
 export default handler
